fddec3d81f27041ae855a3b68bb9ed2e9951fdee
Andy Edwards <andyhedwards@gmail.com>
2015-09-16 18:51:42 +1200

    Add Show and Delete all users
    Add administrative users. Add admin_user method to users_controller.
    Add Pagination gem.

--- spec/features/edit_user_spec.rb
+++ spec/features/edit_user_spec.rb
@@ -1,25 +1,5 @@
 require 'rails_helper'
 
-  feature "Edit form" do
-
-    it "doesnt create user if any field is invalid" do
-      visit edit_user_path
-      click_button("Save changes")
-      expect(view).to render_template(:edit)
-      end
-
-      # it "creates a user if sign up info is valid" do
-      #   fill_in('Name', :with => 'Andy')
-      #   fill_in('Email', :with => 'andy@gmail.com')
-      #   fill_in('Password', :with => 'password')
-      #   fill_in('Confirmation', :with => 'password')
-      #   expect {
-      #     click_button("Create my account")
-      #   }.to change{User.count}.by(+1)
-      # end
-
-
-  end
 
 
 
